3031d3cdd2566660c0cbd8f5b35c05257b6b3dae
Hecarrah <pete.porttinen@gmail.com>
2017-04-09 16:48:45 +0300

    tehtävä 9

--- Viikko4/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ Viikko4/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -13,22 +13,25 @@ import cucumber.api.java.Before;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class Stepdefs {
+
     WebDriver driver;
     String baseUrl = "http://localhost:4567";
-    
+
     @Before
     public void setUp() {
-    ChromeDriverManager.getInstance().setup();
-    driver = new ChromeDriver();
-    System.setProperty("webdriver.chrome.driver", "\"D:\\User\\Tiedostot\\GitHub\\OhTu\\Viikko4\\WebLogin\\chromedriver.exe\"");
-}
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "\"D:\\User\\Tiedostot\\GitHub\\OhTu\\Viikko4\\WebLogin\\chromedriver.exe\"");
+    }
+
     @Given("^login is selected$")
     public void login_selected() throws Throwable {
         driver.get(baseUrl);
-        WebElement element = driver.findElement(By.linkText("login"));       
-        element.click();          
-    } 
-        @Given("^new user is selected$")
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+    }
+
+    @Given("^new user is selected$")
     public void user_selected() throws Throwable {
         driver.get(baseUrl);
         WebElement element = driver.findElement(By.linkText("register new user"));
@@ -42,18 +45,19 @@ public class Stepdefs {
         element = driver.findElement(By.name("password"));
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
-        element.submit();  
+        element.submit();
     }
 
     @Then("^system will respond \"([^\"]*)\"$")
     public void system_will_respond(String pageContent) throws Throwable {
         assertTrue(driver.getPageSource().contains(pageContent));
     }
-    
+
     @When("^correct username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void username_correct_and_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
+
     @When("^correct username \"([^\"]*)\" and password \"([^\"]*)\" is entered$")
     public void new_user_credentials_are_given(String username, String password) throws Throwable {
         createUser(username, password);
@@ -63,68 +67,87 @@ public class Stepdefs {
     public void username_and_incorrect_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
+
     @When("^nonexistent username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void nonexistent_username_and_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
-    
+
     @Then("^user is logged in$")
     public void user_is_logged_in() throws Throwable {
         pageHasContent("Ohtu Application main page");
     }
-    
+
     @Then("^user is not logged in and error message is given$")
     public void user_is_not_logged_in_and_error_message_is_given() throws Throwable {
         pageHasContent("invalid username or password");
         pageHasContent("Give your credentials to login");
     }
+
     @Then("^user is not created and error \"password can not contain only letters\" is reported$")
     public void user_is_not_created_and_error_is_reported() throws Throwable {
         pageHasContent("password can not contain only letters");
     }
+
     @Then("^user is not created and error \"username is already taken\" is reported$")
     public void not_created_existing() throws Throwable {
         pageHasContent("username is already taken");
     }
+
     @Then("^user is not created and error \"password should have at least 8 characters\" is reported$")
     public void not_created_password() throws Throwable {
         pageHasContent("password should have at least 8 characters");
     }
+
     @Then("^user is not created and error \"username should have at least 3 characters\" is reported$")
     public void not_created() throws Throwable {
         pageHasContent("username should have at least 3 characters");
     }
-    
-    
+
     @When("^username \"([^\"]*)\" and password \"([^\"]*)\" is entered")
     public void username_and_password_are_entered(String username, String password) throws Throwable {
         createUser(username, password);
     }
+
     @When("^username \"([^\"]*)\" and password \"([^\"]*)\" and incorrect confirmation \"([^\"]*)\" is entered")
     public void username_and_password_are_entered_but_confirmation_wrong(String arg1, String arg2,
             String arg3) throws Throwable {
         createUserPConf(arg1, arg2, arg3);
     }
+
     @Then("^user is not created and error \"password and password confirmation do not match\" is given")
     public void user_is_not_created_password_confirmation() throws Throwable {
         pageHasContent("password and password confirmation do not match");
     }
+
     @Then("^new user is created$")
     public void user_is_created() throws Throwable {
         pageHasContent("Welcome to Ohtu Application!");
     }
-    
+
+    @Given("^user with username \"([^\"]*)\" with password \"([^\"]*)\" is successfully created$")
+    public void user_created(String username, String password) throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        createUser(username, password);
+    }
+
+    @Given("^user with username \"([^\"]*)\" and password \"([^\"]*)\" is unsuccessfully created$")
+    public void user_not_created(String username, String password) throws Throwable {
+        user_created(username, password);
+    }
+
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
-        
+
     /* helper methods */
- 
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
     }
-        
+
     private void logInWith(String username, String password) {
         assertTrue(driver.getPageSource().contains("Give your credentials to login"));
         WebElement element = driver.findElement(By.name("username"));
@@ -132,8 +155,9 @@ public class Stepdefs {
         element = driver.findElement(By.name("password"));
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
-        element.submit();  
-    } 
+        element.submit();
+    }
+
     private void createUser(String username, String password) {
         WebElement element = driver.findElement(By.name("username"));
         element.sendKeys(username);
--- Viikko4/WebLogin/src/test/resources/ohtu/new_user.feature
+++ Viikko4/WebLogin/src/test/resources/ohtu/new_user.feature
@@ -29,3 +29,15 @@ Feature: A new user account can be created if a proper unused username and passw
         Given new user is selected
         When username "Eero4" and password "oree2" and incorrect confirmation "mistake" is entered
         Then user is not created and error "password and password confirmation do not match" is given  
+
+    Scenario: user can login with successfully generated account
+        Given user with username "liisa" with password "salainen1" is successfully created
+        And   login is selected
+        When  correct username "liisa" and incorrect password "salainen1" are given
+        Then  user is logged in 
+
+    Scenario: user can not login with account that is not successfully created
+        Given user with username "aa" and password "bad" is unsuccessfully created
+        And   login is selected
+        When  nonexistent username "aa" and password "bad" are given
+        Then  user is not logged in and error message is given
